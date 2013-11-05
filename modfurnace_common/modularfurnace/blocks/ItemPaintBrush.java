ecb2b939a0ad751c38a4dc7213b8750183423c6b
pauljoda <pauldavis101@gmail.com>
2013-11-04 21:50:49 -0600

    Fun with dev items. Easy furnace swapping with the paintbrush

--- modfurnace_common/modularfurnace/blocks/ItemPaintBrush.java
+++ modfurnace_common/modularfurnace/blocks/ItemPaintBrush.java
@@ -11,12 +11,13 @@ import net.minecraft.world.World;
 
 public class ItemPaintBrush extends Item {
 	int block = 0;
+	int location = 0;
 
 	public ItemPaintBrush(int par1) {
 		super(par1);
 		setCreativeTab(ModularFurnace.tabModularFurnace);
 		this.setUnlocalizedName("paintBrush");
-		
+
 	}
 	@Override
 	@SideOnly(Side.CLIENT)
@@ -25,13 +26,35 @@ public class ItemPaintBrush extends Item {
 
 	}
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
-    {
-		
+	{
+
 		if(par2EntityPlayer.isSneaking())
-			block  = par3World.getBlockId(par4, par5, par6);
-		
+			if(!par3World.blockHasTileEntity(par4, par5, par6))
+				block  = par3World.getBlockId(par4, par5, par6);
+
+		if(!par2EntityPlayer.isSneaking())
+		{
+			if(!par3World.blockHasTileEntity(par4, par5, par6))
+			{
+				location = par3World.getBlockId(par4, par5, par6);
+				for(int depth = -2; depth <= 2; depth++)
+				{
+					for(int row = -2; row <=2; row++)
+					{
+						for(int line = -2; line <= 2; line++)
+						{
+							if(par3World.getBlockId(par4 + row, par5 + depth, par6 + line) == location)
+							{
+								par3World.setBlock(par4 + row, par5 + depth, par6 + line, block);
+							}
+						}
+					}
+				}
+			}
+		}
+
 		par3World.setBlock(par4, par5, par6, block);
-		
+
 		return false;
-    }
+	}
 }
